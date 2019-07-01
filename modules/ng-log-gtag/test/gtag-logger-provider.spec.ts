9bb501add9564d76583098c2a6cd55ac8567a5b4
Moe Myat Zaw <mmzliveid@gmail.com>
2019-07-01 22:44:33 +0630

    Update project

--- modules/ng-log-gtag/test/gtag-logger-provider.spec.ts
+++ modules/ng-log-gtag/test/gtag-logger-provider.spec.ts
@@ -1,141 +1,122 @@
-// // tslint:disable: no-floating-promises
+// tslint:disable: no-floating-promises
+import { TestBed } from '@angular/core/testing';
 
-// import { TestBed } from '@angular/core/testing';
+import { LogLevel } from '@dagonmetric/ng-log';
 
-// import { LogLevel } from '@dagonmetric/ng-log';
-// import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { GTAG_LOGGER_OPTIONS, GTagLogger } from '../src/gtag-logger';
+import { GTagLoggerProvider } from '../src/gtag-logger-provider';
 
-// import { ApplicationInsightsLogger } from '../src/applicationinsights-logger';
-// import { APPLICATIONINSIGHTS_LOGGER_OPTIONS, ApplicationInsightsLoggerProvider } from '../src/applicationinsights-logger-provider';
+// tslint:disable-next-line: no-any
+declare var window: any;
 
-// describe('ApplicationInsightsLoggerProvider', () => {
-//     let loggerProvider: ApplicationInsightsLoggerProvider;
+describe('GTagLoggerProvider', () => {
+    let loggerProvider: GTagLoggerProvider;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             providers: [
-//                 ApplicationInsightsLoggerProvider,
-//                 {
-//                     provide: APPLICATIONINSIGHTS_LOGGER_OPTIONS,
-//                     useValue: { config: {
-//                         instrumentationKey: 'TEST'
-//                     } }
-//                 }
-//             ]
-//         });
+    beforeEach(() => {
+        // tslint:disable-next-line: no-unsafe-any
+        window.gtag = jasmine.createSpy('gtag');
 
-//         loggerProvider =
-//             TestBed.get<ApplicationInsightsLoggerProvider>(ApplicationInsightsLoggerProvider) as ApplicationInsightsLoggerProvider;
+        TestBed.configureTestingModule({
+            providers: [
+                GTagLoggerProvider,
+                {
+                    provide: GTAG_LOGGER_OPTIONS,
+                    useValue: {
+                        measurementId: 'UA-111111111-1',
+                        customMap: {
+                            dimension1: 'key1'
+                        }
+                    }
+                }
+            ]
+        });
 
-//     });
+        loggerProvider =
+            TestBed.get<GTagLoggerProvider>(GTagLoggerProvider) as GTagLoggerProvider;
 
-//     it('should be created', () => {
-//         expect(loggerProvider).toBeDefined();
-//         expect(loggerProvider.name).toBe('applicationinsights');
-//     });
+    });
 
-//     it("should create a new logger instance with 'createLogger' method", () => {
-//         const logger = loggerProvider.createLogger('test');
-//         expect(logger instanceof ApplicationInsightsLogger).toBeTruthy();
-//         expect((logger as ApplicationInsightsLogger).name).toBe('test');
-//     });
+    it('should be created', () => {
+        expect(loggerProvider).toBeDefined();
+        expect(loggerProvider.name).toBe('gtag');
+    });
 
-//     it("should work with 'setUserProperties'", () => {
-//         const appInsights = loggerProvider.appInsights as ApplicationInsights;
-//         loggerProvider.setConfig({
-//             instrumentationKey: 'TESTING'
-//         });
-//         expect(appInsights.config.instrumentationKey).toBe('TESTING');
-//     });
+    it("should create a new logger instance with 'createLogger' method", () => {
+        const logger = loggerProvider.createLogger('test');
+        expect(logger instanceof GTagLogger).toBeTruthy();
+        expect((logger as GTagLogger).name).toBe('test');
+    });
 
-//     it("should work with 'setUserProperties'", () => {
-//         const appInsights = loggerProvider.appInsights as ApplicationInsights;
-//         spyOn(appInsights, 'setAuthenticatedUserContext');
+    it("should work with 'log'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'log');
 
-//         const userId = 'user1';
-//         const accountId = 'account1';
-//         loggerProvider.setUserProperties(userId, accountId);
-//         expect(appInsights.setAuthenticatedUserContext).toHaveBeenCalledWith(userId, accountId);
-//     });
+        const logLevel = LogLevel.Info;
+        const msg = 'This is a message.';
+        const logInfo = { properties: { key1: 'value1' } };
+        loggerProvider.log(logLevel, msg, logInfo);
+        expect(currentLogger.log).toHaveBeenCalledWith(logLevel, msg, logInfo);
+    });
 
-//     it("should work with 'clearUserProperties'", () => {
-//         const appInsights = loggerProvider.appInsights as ApplicationInsights;
-//         spyOn(appInsights, 'clearAuthenticatedUserContext');
+    it("should work with 'startTrackPage'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'startTrackPage');
 
-//         loggerProvider.clearUserProperties();
-//         expect(appInsights.clearAuthenticatedUserContext).toHaveBeenCalled();
-//     });
+        loggerProvider.startTrackPage('page1');
+        expect(currentLogger.startTrackPage).toHaveBeenCalledWith('page1');
+    });
 
-//     it("should work with 'log'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'log');
+    it("should work with 'stopTrackPage'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'stopTrackPage');
 
-//         const logLevel = LogLevel.Info;
-//         const msg = 'This is a message.';
-//         const logInfo = { properties: { key1: 'value1' } };
-//         loggerProvider.log(logLevel, msg, logInfo);
-//         expect(currentLogger.log).toHaveBeenCalledWith(logLevel, msg, logInfo);
-//     });
+        const name = 'page1';
+        const pageViewInfo = { uri: '/home' };
+        loggerProvider.stopTrackPage(name, pageViewInfo);
+        expect(currentLogger.stopTrackPage).toHaveBeenCalledWith(name, pageViewInfo);
+    });
 
-//     it("should work with 'startTrackPage'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'startTrackPage');
+    it("should work with 'trackPageView'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'trackPageView');
 
-//         loggerProvider.startTrackPage('page1');
-//         expect(currentLogger.startTrackPage).toHaveBeenCalledWith('page1');
-//     });
+        const pageViewInfo = { name: 'page1', uri: '/home' };
+        loggerProvider.trackPageView(pageViewInfo);
+        expect(currentLogger.trackPageView).toHaveBeenCalledWith(pageViewInfo);
+    });
 
-//     it("should work with 'stopTrackPage'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'stopTrackPage');
+    it("should work with 'startTrackEvent'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'startTrackEvent');
 
-//         const name = 'page1';
-//         const pageViewInfo = { uri: '/home' };
-//         loggerProvider.stopTrackPage(name, pageViewInfo);
-//         expect(currentLogger.stopTrackPage).toHaveBeenCalledWith(name, pageViewInfo);
-//     });
+        loggerProvider.startTrackEvent('event1');
+        expect(currentLogger.startTrackEvent).toHaveBeenCalledWith('event1');
+    });
 
-//     it("should work with 'trackPageView'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'trackPageView');
+    it("should work with 'stopTrackEvent'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'stopTrackEvent');
 
-//         const pageViewInfo = { name: 'page1', uri: '/home' };
-//         loggerProvider.trackPageView(pageViewInfo);
-//         expect(currentLogger.trackPageView).toHaveBeenCalledWith(pageViewInfo);
-//     });
+        const name = 'event1';
+        const eventInfo = { event_category: 'test' };
+        loggerProvider.stopTrackEvent(name, eventInfo);
+        expect(currentLogger.stopTrackEvent).toHaveBeenCalledWith(name, eventInfo);
+    });
 
-//     it("should work with 'startTrackEvent'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'startTrackEvent');
+    it("should work with 'trackEvent'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'trackEvent');
 
-//         loggerProvider.startTrackEvent('event1');
-//         expect(currentLogger.startTrackEvent).toHaveBeenCalledWith('event1');
-//     });
+        const eventInfo = { name: 'event1', eventCategory: 'test' };
+        loggerProvider.trackEvent(eventInfo);
+        expect(currentLogger.trackEvent).toHaveBeenCalledWith(eventInfo);
+    });
 
-//     it("should work with 'stopTrackEvent'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'stopTrackEvent');
+    it("should work with 'flush'", () => {
+        const currentLogger = loggerProvider.currentLogger;
+        spyOn(currentLogger, 'flush');
 
-//         const name = 'event1';
-//         const eventInfo = { eventCategory: 'test' };
-//         loggerProvider.stopTrackEvent(name, eventInfo);
-//         expect(currentLogger.stopTrackEvent).toHaveBeenCalledWith(name, eventInfo);
-//     });
-
-//     it("should work with 'trackEvent'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'trackEvent');
-
-//         const eventInfo = { name: 'event1', eventCategory: 'test' };
-//         loggerProvider.trackEvent(eventInfo);
-//         expect(currentLogger.trackEvent).toHaveBeenCalledWith(eventInfo);
-//     });
-
-//     it("should work with 'flush'", () => {
-//         const currentLogger = loggerProvider.currentLogger;
-//         spyOn(currentLogger, 'flush');
-
-//         loggerProvider.flush();
-//         expect(currentLogger.flush).toHaveBeenCalled();
-//     });
-// });
+        loggerProvider.flush();
+        expect(currentLogger.flush).toHaveBeenCalled();
+    });
+});
--- modules/ng-log-gtag/test/gtag-logger.module.spec.ts
+++ modules/ng-log-gtag/test/gtag-logger.module.spec.ts
@@ -1,47 +1,41 @@
-// // tslint:disable: no-floating-promises
+// tslint:disable: no-floating-promises
 
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { LOGGER_PROVIDER, LoggerProvider } from '@dagonmetric/ng-log';
+import { LOGGER_PROVIDER, LoggerProvider } from '@dagonmetric/ng-log';
 
-// import {
-//     APPLICATIONINSIGHTS_LOGGER_OPTIONS,
-//     ApplicationInsightsLoggerOptions,
-//     ApplicationInsightsLoggerProvider
-// } from '../src/applicationinsights-logger-provider';
-// import { ApplicationInsightsLoggerModule } from '../src/applicationinsights-logger.module';
+import { GTAG_LOGGER_OPTIONS, GTagLoggerOptions } from '../src/gtag-logger';
+import { GTagLoggerProvider } from '../src/gtag-logger-provider';
+import { GTagLoggerModule } from '../src/gtag-logger.module';
 
-// describe('ApplicationInsightsLoggerModule', () => {
-//     it("should provide 'ApplicationInsightsLoggerProvider'", () => {
-//         TestBed.configureTestingModule({
-//             imports: [
-//                 ApplicationInsightsLoggerModule
-//             ]
-//         });
+describe('GTagLoggerModule', () => {
+    it("should provide 'GTagLoggerProvider'", () => {
+        TestBed.configureTestingModule({
+            imports: [
+                GTagLoggerModule
+            ]
+        });
 
-//         const loggerProviders = TestBed.get<ApplicationInsightsLoggerProvider[]>(LOGGER_PROVIDER);
+        const loggerProviders = TestBed.get<GTagLoggerProvider[]>(LOGGER_PROVIDER);
 
-//         expect(loggerProviders).toBeDefined();
-//         expect((loggerProviders as LoggerProvider[])[0] instanceof ApplicationInsightsLoggerProvider).toBeTruthy();
-//     });
+        expect(loggerProviders).toBeDefined();
+        expect((loggerProviders as LoggerProvider[])[0] instanceof GTagLoggerProvider).toBeTruthy();
+    });
 
-//     describe('withOptions', () => {
-//         it("should provide 'APPLICATIONINSIGHTS_LOGGER_OPTIONS' value", () => {
-//             TestBed.configureTestingModule({
-//                 imports: [
-//                     ApplicationInsightsLoggerModule.withOptions({
-//                         config: {
-//                             instrumentationKey: 'TEST'
-//                         }
-//                     })
-//                 ]
-//             });
+    describe('withOptions', () => {
+        it("should provide 'GTAG_LOGGER_OPTIONS' value", () => {
+            TestBed.configureTestingModule({
+                imports: [
+                    GTagLoggerModule.withOptions({
+                        measurementId: 'UA-111111111-1'
+                    })
+                ]
+            });
 
-//             const options = TestBed
-//                 .get<ApplicationInsightsLoggerOptions>(APPLICATIONINSIGHTS_LOGGER_OPTIONS) as ApplicationInsightsLoggerOptions;
+            const options = TestBed.get<GTagLoggerOptions>(GTAG_LOGGER_OPTIONS) as GTagLoggerOptions;
 
-//             expect(options).toBeDefined();
-//             expect(options.config.instrumentationKey).toBe('TEST');
-//         });
-//     });
-// });
+            expect(options).toBeDefined();
+            expect(options.measurementId).toBe('UA-111111111-1');
+        });
+    });
+});
