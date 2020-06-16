448dfb91a7f56211f5b7ece786965d6ccce25f55
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-16 23:01:44 +0630

    Update gtag.ts

--- modules/ng-log-gtag/src/gtag.ts
+++ modules/ng-log-gtag/src/gtag.ts
@@ -1,1 +1,9 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
 export type GTag = (command: 'event' | 'config' | 'set', actionOrProperties: unknown, properties?: unknown) => void;
