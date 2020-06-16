63c81c001e5c5d1333b871d86ef47440e4d1fef1
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-16 22:48:16 +0630

    Update karma.conf.js

--- modules/ng-log-gtag/karma.conf.js
+++ modules/ng-log-gtag/karma.conf.js
@@ -1,9 +1,12 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
-    const puppeteer = require('puppeteer');
-    process.env.CHROME_BIN = puppeteer.executablePath();
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable  */
+
+module.exports = (config) => {
+    // const puppeteer = require('puppeteer');
+    // process.env.CHROME_BIN = puppeteer.executablePath();
 
     config.set({
         basePath: '',
@@ -20,7 +23,7 @@ module.exports = function (config) {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../../dist/coverage/ng-log-gtag'),
+            dir: require('path').join(__dirname, '../../coverage/ng-log-gtag'),
             reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
             fixWebpackSourcePaths: true,
             thresholds: {
@@ -32,19 +35,19 @@ module.exports = function (config) {
         },
         reporters: ['progress', 'kjhtml'],
         junitReporter: {
-            outputDir: '../../dist/junit/ng-log-gtag'
+            outputDir: '../../junit/ng-log-gtag'
         },
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['Chrome'],
-        // customLaunchers: {
-        //     ChromeHeadlessCI: {
-        //         base: 'ChromeHeadless',
-        //         flags: ['--no-sandbox']
-        //     }
-        // },
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
         singleRun: false,
         restartOnFileChange: true
     });
