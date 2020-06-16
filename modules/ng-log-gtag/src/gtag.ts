99d1070404fc03b3a0020d2e2a001b950e7000c0
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-16 22:52:24 +0630

    Update gtag.ts

--- modules/ng-log-gtag/src/gtag.ts
+++ modules/ng-log-gtag/src/gtag.ts
@@ -1,1 +1,1 @@
-export type GTag = (command: 'event' | 'config' | 'set', actionOrProperties: any, properties?: any) => void;
+export type GTag = (command: 'event' | 'config' | 'set', actionOrProperties: unknown, properties?: unknown) => void;
